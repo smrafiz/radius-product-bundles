2b17597f52baea8d656745a728acc266b55787f1
smrafiz <s.m.rafiz@gmail.com>
2025-08-06 16:46:40 +0600

    gitignore refreshed

--- web/next.config.js
+++ web/next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
         NEXT_PUBLIC_HOST: process.env.HOST,
         NEXT_PUBLIC_SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY,
     },
-    allowedDevOrigins: [
-        process.env.SHOPIFY_APP_URL.toString().replace("https://", ""),
-    ],
 };
 
 module.exports = nextConfig;
