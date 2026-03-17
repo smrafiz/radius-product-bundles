09183f829e3f796a6587a4c41f38ea54236c0785
smrafiz <s.m.rafiz@gmail.com>
2026-03-17 14:44:19 +0600

    feat(support): add support constants (URLs, FAQ count, quick links)

---
+++ web/features/support/constants/support.constants.ts
@@ -0,0 +1,13 @@
+export const SUPPORT_EMAIL = "support@radiusbundles.com";
+export const DOCS_URL = "https://docs.radiusbundles.com";
+export const VIDEOS_URL = "https://youtube.com/@radiusbundles";
+export const APP_STORE_URL = "https://apps.shopify.com/radius-product-bundles";
+export const COMMUNITY_URL = "https://community.radiusbundles.com";
+export const FAQ_COUNT = 5;
+
+export const QUICK_LINKS = [
+    { key: "linkDocs",      url: DOCS_URL,       external: true },
+    { key: "linkVideos",    url: VIDEOS_URL,      external: true },
+    { key: "linkAppStore",  url: APP_STORE_URL,   external: true },
+    { key: "linkCommunity", url: COMMUNITY_URL,   external: true },
+] as const;
