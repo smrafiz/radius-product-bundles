a02a55e2a3c1fa88ba826badf44a3c10df737490
liputech <81138435+liputech@users.noreply.github.com>
2026-04-09 14:35:54 +0600

    fix code rearange

--- web/features/pricing/components/pricing-card/pricing-card.tsx
+++ web/features/pricing/components/pricing-card/pricing-card.tsx
@@ -42,14 +42,10 @@ export const PricingCard = () => {
                         variant={!isMonthly ? "primary" : "tertiary"}
                         onClick={() => setBillingInterval("ANNUAL")}
                     >
-                        {/*{t("billingAnnual")}*/}
-                    {/*    <span className={`flex align-center font-medium ${!isMonthly ? "text-white" : "text-gray-400"}`}>*/}
-                    {/*{t("billingAnnual")}{" "}*/}
-                            <s-stack gap="small-300" direction="inline">
-                                <span className={`text-[12px] font-[550] ${!isMonthly ? "text-white" : ""}`}>{t("billingAnnual")}</span>
-                                <s-badge tone="success">{t("annualSavings", { percent: String(annualSavingsPercent) })}</s-badge>
-                            </s-stack>
-                {/*</span>*/}
+                        <s-stack gap="small-300" direction="inline">
+                            <span className={`text-[12px] font-[550] ${!isMonthly ? "text-white" : ""}`}>{t("billingAnnual")}</span>
+                            <s-badge tone="success">{t("annualSavings", { percent: String(annualSavingsPercent) })}</s-badge>
+                        </s-stack>
                     </s-button>
                 </s-stack>
             </s-stack>
