b04caae834e5b6b871a6c4c01eb0652a81d61cab
Ubuntu <ubuntu@ip-172-31-27-41.ap-south-1.compute.internal>
2026-04-12 07:09:52 +0000

    Update ecosytem

--- web/ecosystem.config.cjs
+++ web/ecosystem.config.cjs
@@ -6,11 +6,16 @@ module.exports = {
             args: "start -p 5000",
             cwd: "/var/www/shopify/web",
             instances: 1,
-            exec_mode: "fork",
+            exec_mode: "cluster",
             env: {
                 NODE_ENV: "production",
                 PORT: 5000,
             },
-        },
-    ],
+            max_memory_restart: "1G",
+	    listen_timeout: 10000,
+            kill_timeout: 5000,
+            exp_backoff_restart_delay: 100
+            log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+        }
+    ]
 };
